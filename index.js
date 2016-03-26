314d58b4dd321fbc4ab677213da8bf0ee6cb3ded
KleoPetroff <kleopetroff@gmail.com>
2016-03-26 11:17:49 +0200

    add babel-plugin-transform-class-properties

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ module.exports = {
     require('babel-plugin-syntax-class-properties'),
     require('babel-plugin-syntax-decorators'),
     require('babel-plugin-syntax-object-rest-spread'),
-    
+
+    require('babel-plugin-transform-class-properties'),
     require('babel-plugin-transform-object-assign'),
     require('babel-plugin-transform-object-rest-spread'),
     require('babel-plugin-transform-decorators-legacy').default
